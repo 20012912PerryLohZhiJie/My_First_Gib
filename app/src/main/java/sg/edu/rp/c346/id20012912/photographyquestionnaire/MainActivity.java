bfedef7fd3f8cd79b71056c621432e19874d02ab
Perry Loh <20012912@myrp.edu.sg>
2021-04-28 10:26:17 +0800

    Instill commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20012912/photographyquestionnaire/MainActivity.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id20012912.photographyquestionnaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //comment
+
+    }
+}
